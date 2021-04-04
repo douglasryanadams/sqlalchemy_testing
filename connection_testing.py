3570d1c5b44aa6493b20fa006ae194250fcea23b
Douglas Adams <douglasryanadams@gmail.com>
2021-04-04 14:27:31 -0700

    Adds data

--- connection_testing.py
+++ connection_testing.py
@@ -1,26 +1,31 @@
 import logging
 
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine, Table, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 from tabulate import tabulate
 
 LOGGER = logging.getLogger(__name__)
 
+
+# Set up database
+
 CONNECTION_URL = 'postgresql://postgres:postgres@localhost:5432/postgres'
 engine = create_engine(CONNECTION_URL)
+Base = declarative_base()
 Session = sessionmaker(autoflush=False, bind=engine)
 
-meta = MetaData()
-test_data = Table(
-    'test_data', meta,
-    Column('id', Integer, primary_key = True),
-    Column('name', String),
-    Column('count', Integer),
-)
+class TestData(Base):
+    __tablename__ = 'test_data'
+    test_data_id = Column('id', Integer, primary_key = True)
+    name = Column('name', String)
+    count = Column('count', Integer)
 
-meta.create_all(engine)
+Base.metadata.create_all(engine)
 
+
+# Run Tests
 
 def test_connection():
     s = Session()
@@ -28,6 +33,23 @@ def test_connection():
     _print_table('test_connection', r)
 
 
+def test_crud():
+    s = Session()
+    try:
+        for i in range(0,10):
+            td = TestData(name=f'name_{i}', count=22)
+            s.add(td)
+        s.commit()
+        r = s.execute('SELECT * FROM test_data')
+        _print_table('test_crud', r)
+
+        s.execute('DELETE FROM test_data')
+        s.commit()
+    finally:
+        s.close()
+
+
+# Convenience Methods
 
 def _print_table(test_name, result):
     data = result.fetchall()
